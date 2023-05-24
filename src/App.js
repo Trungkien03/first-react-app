edd00ff64ae16604be8d3ca61d3cc17d0fe85172
Kien Nguyen <trungkiennguyen0310@gmail.com>
2023-05-24 17:17:37 +0700

    useEffect Exercise

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import '../src/CSS/App.css';
 import Navigation from './components/Navigation';
 import Player from './components/Players';
 import Footer from './components/Footer';
-import Main from './components/main';
 function App() {
   return (
     <div className='App'>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,72 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import { useContext } from 'react';
+import { ThemeContext } from './ThemeContext';
+
 export default function Navigation() {
-    return(
-        <nav>
-            <ul>
-                <li><a className='active' href='#home'>Home</a></li>
-               <li><a href='#news'>News</a></li>
-               <li><a href='#about'>About</a></li>
-               <li><a href='contact'>Contact</a></li>
-           </ul>
-       </nav>
-    )
+  const { theme, toggle, dark } = useContext(ThemeContext);
+  const [activeLink, setActiveLink] = useState('home');
+
+  const handleLinkClick = (link) => {
+    setActiveLink(link);
+  };
+
+  return (
+    <div>
+      <nav style={{ backgroundColor: theme.backgroundColor, color: theme.color }}>
+        <ul>
+          <li>
+            <a
+              className={activeLink === 'home' ? 'active' : ''}
+              href='#home'
+              onClick={() => handleLinkClick('home')}
+            >
+              Home
+            </a>
+          </li>
+          <li>
+            <a
+              className={activeLink === 'news' ? 'active' : ''}
+              href='#news'
+              onClick={() => handleLinkClick('news')}
+            >
+              News
+            </a>
+          </li>
+          <li>
+            <a
+              className={activeLink === 'about' ? 'active' : ''}
+              href='#about'
+              onClick={() => handleLinkClick('about')}
+            >
+              About
+            </a>
+          </li>
+          <li>
+            <a
+              className={activeLink === 'contact' ? 'active' : ''}
+              href='#contact'
+              onClick={() => handleLinkClick('contact')}
+            >
+              Contact
+            </a>
+          </li>
+        </ul>
+        <div style={{ position: 'relative' }}>
+          <a
+            className='switch-mode'
+            href='#'
+            onClick={toggle}
+            style={{
+              backgroundColor: theme.backgroundColor,
+              color: theme.color,
+              outline: 'none',
+            }}
+            data-testid='toggle-theme-btn'
+          >
+            Switch Nav to {!dark ? 'Dark' : 'Light'} mode
+          </a>
+        </div>
+      </nav>
+    </div>
+  );
 }
